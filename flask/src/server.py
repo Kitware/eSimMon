9de078c6077e73e1bb3fe18103a238a5d16ce448
Brianna Major <brianna.major@kitware.com>
2020-04-24 16:58:10 -0400

    Get token and id from client
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- flask/src/server.py
+++ flask/src/server.py
@@ -2,21 +2,27 @@ import ffmpeg
 import tempfile
 
 from girder_client import GirderClient
-from flask import Flask, send_file, request
+from flask import Flask, send_file, request, Response
 from flask_cors import CORS
 
 app = Flask(__name__)
 
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-@app.route('/plots/api/movie', methods=['GET'])
+@app.route('/api/movie/<id>', methods=['GET'])
 def create_movie(id):
+    token = request.headers.get('girderToken', '')
+    if not token or not id:
+        return Response('Invalid token or parameter ID.', status=400, mimetype='application/json')
+
     gc = GirderClient(apiUrl='https://data.kitware.com/api/v1/')
-    gc.setToken("Jka8QlMSkeMIAipeq1EBpQ9GHKBqcE78tsbQVdnHhzM2Wn9unT8B1li4FHKnstyM")
+    gc.setToken(token)
     output_file = tempfile.NamedTemporaryFile(suffix='.mp4', delete=True)
     with tempfile.TemporaryDirectory() as tmpdir:
-        item = gc.getItem('5e87d2232660cbefba89703e')
-        gc.downloadItem('5e87d2232660cbefba89703e', tmpdir, item['name'])
+        item = gc.getItem(id)
+        if not item:
+            return Response('Unable to fetch item', status=400, mimetype='application/json')
+        gc.downloadItem(id, tmpdir, item['name'])
         path = tmpdir + '/' + item['name'] + '/*.svg'
         ffmpeg.input(path, pattern_type='glob', framerate=1).output(output_file.name).overwrite_output().run()
     return send_file(output_file, attachment_filename=item['name']+'.mp4')
