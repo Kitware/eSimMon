919479b9f30e28bb01c68532c02e8d91df3a8df3
Brianna Major <brianna.major@kitware.com>
2020-04-28 11:37:28 -0400

    Don't make unnecessary api calls
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- flask/src/server.py
+++ flask/src/server.py
@@ -8,27 +8,27 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app, expose_headers=["x-suggested-filename"])
 
 @app.route('/api/movie/<id>', methods=['GET'])
 def create_movie(id):
     token = request.headers.get('girderToken', '')
     if not token or not id:
-        return Response('Invalid token or parameter ID.', status=400, mimetype='application/json')
+        return Response('Invalid token or parameter ID.', status=400)
 
     gc = GirderClient(apiUrl='https://data.kitware.com/api/v1/')
     gc.setToken(token)
     output_file = tempfile.NamedTemporaryFile(suffix='.mp4', delete=True)
     with tempfile.TemporaryDirectory() as tmpdir:
-        item = gc.getItem(id)
-        if not item:
-            return Response('Unable to fetch item', status=400, mimetype='application/json')
-        gc.downloadItem(id, tmpdir, item['name'])
-        path_name = os.path.join(tmpdir, item['name'], '*.svg')
-        ffmpeg.input(path_name, pattern_type='glob', framerate=1).output(output_file.name).overwrite_output().run()
-    response = make_response(send_file(output_file, attachment_filename=item['name']+'.mp4'))
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+        gc.downloadItem(id, tmpdir)
+        item_name = os.listdir(tmpdir)[0]
+        path_name = os.path.join(tmpdir, item_name, '*.svg')
+        (ffmpeg
+            .input(path_name, pattern_type='glob', framerate=1)
+            .output(output_file.name)
+            .overwrite_output()
+            .run())
+    return send_file(output_file, attachment_filename=item_name+'.mp4')
 
 
 if __name__ == '__main__':
