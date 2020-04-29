b3a7d482e82a42afbf2c8d23755751f6b6931683
Brianna Major <brianna.major@kitware.com>
2020-04-29 11:18:37 -0400

    Clean up temp output file with temp directory
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- flask/src/server.py
+++ flask/src/server.py
@@ -3,7 +3,7 @@ import tempfile
 import os
 
 from girder_client import GirderClient
-from flask import Flask, send_file, request, Response
+from flask import Flask, request, Response, send_file
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -18,24 +18,19 @@ def create_movie(id):
 
     gc = GirderClient(apiUrl='https://data.kitware.com/api/v1/')
     gc.setToken(token)
-    output_file = tempfile.NamedTemporaryFile(suffix='.mp4', delete=True)
     with tempfile.TemporaryDirectory() as tmpdir:
         gc.downloadItem(id, tmpdir)
         item_name = os.listdir(tmpdir)[0]
         path_name = os.path.join(tmpdir, item_name, '*.svg')
+        output_file = tempfile.NamedTemporaryFile(suffix='.mp4')
         (ffmpeg
             .input(path_name, pattern_type='glob', framerate=1)
             .output(output_file.name)
             .overwrite_output()
             .run())
 
-    # Make sure temp file is deleted after it is downloaded
-    def stream_and_remove_file():
-        yield from output_file
-        output_file.close()
-
-    return Response(stream_and_remove_file(), mimetype='mp4')
+        return send_file(output_file, mimetype='mp4')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
