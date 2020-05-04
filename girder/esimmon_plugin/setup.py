495c4582a55a6b8d11222987337af84a30c9f296
Brianna Major <brianna.major@kitware.com>
2020-05-04 11:00:44 -0400

    Update the plugin description
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- girder/esimmon_plugin/setup.py
+++ girder/esimmon_plugin/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='esimmon-plugin',
     version='0.0.1',
-    description='Finds items matching search parameters.',
+    description='Provide endpoints to support eSimMon dashboard.',
     packages=find_packages(),
     install_requires=[
       'girder>=3.0.0'
