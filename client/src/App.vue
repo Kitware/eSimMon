e8fffc9cc59d4a4528015b304202b952b36d7825
Brianna Major <brianna.major@kitware.com>
2020-05-19 08:51:22 -0400

    Look for default location folder id if one is not provided
    If the client is not being run in production mode and the user has not provided
    a folder id to use as the default location, look for the data folder inside the
    eSimMon collection that the ansible service will have created and use that as
    the default location.
    
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- client/src/App.vue
+++ client/src/App.vue
@@ -414,7 +414,14 @@ export default {
     }
   },
 
-  created: function () {
+  created: async function () {
+    let data = await this.girderRest.get(`/folder/${this.defaultLocation._id}`)
+      .catch(() => {return {}});
+    if (_.isEmpty(data)) {
+      let { data } = await this.girderRest.get(
+        `/resource/lookup?path=%2Fcollection%2FeSimMon%2Fdata`);
+      this.defaultLocation['_id'] = data['_id'];
+    }
     this.$on('data-loaded', this.initialDataLoaded);
     this.$on('gallery-ready', this.incrementReady);
     this.$on('param-selected', this.contextMenu);
