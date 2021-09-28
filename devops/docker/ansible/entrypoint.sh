72e26b9e96730015ce31866cc37e1add1308e644
Brianna Major <brianna.major@kitware.com>
2021-09-28 14:59:01 -0400

    Add SMTP info and auto join rules for registration
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- devops/docker/ansible/entrypoint.sh
+++ devops/docker/ansible/entrypoint.sh
@@ -1,3 +1,10 @@
 ansible-galaxy install -r requirements.yml
 export ANSIBLE_LIBRARY=/root/.ansible/roles/girder/library
-ansible-playbook -i localhost -e admin_password=$ADMIN_PASSWORD site.yml
+ansible-playbook -i localhost \
+-e admin_password=$ADMIN_PASSWORD \
+-e smtp_host=$SMTP_HOST \
+-e smtp_username=$SMTP_USERNAME \
+-e smtp_password=$SMTP_PASSWORD \
+-e email_from=$EMAIL_FROM \
+-e email_host=$EMAIL_HOST \
+site.yml
