fc968f57de270c5c662a0979d470dde94c8864d8
Chris Harris <chris.harris@kitware.com>
2022-04-15 15:38:21 -0400

    Remove unused script

--- fastapi/scripts/offscreen_rendering.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DISPLAY=:99.0
-which Xvfb
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-sleep 3
-exec "$@"
-gunicorn -w 4 --worker-class uvicorn.workers.UvicornWorker -t 600 app.main:app -b 0.0.0.0:5000
