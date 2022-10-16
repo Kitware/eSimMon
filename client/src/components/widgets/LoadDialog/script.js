6787a71f0aef3e547075b6413b71c1bfd45678e1
Brianna Major <brianna.major@kitware.com>
2022-10-16 18:04:25 -0400

    Use composition API for LoadDialog
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- client/src/components/widgets/LoadDialog/script.js
+++ client/src/components/widgets/LoadDialog/script.js
@@ -1,38 +1,15 @@
-import { mapActions, mapGetters, mapMutations } from "vuex";
+import { ref, computed, inject, watch } from "@vue/composition-api";
+import { useActions, useGetters, useMutations } from "vuex-composition-helpers";
 
 export default {
-  inject: ["girderRest"],
+  setup(_props, { emit }) {
+    const girderRest = inject("girderRest");
 
-  data() {
-    return {
-      activeTab: 0,
-      clicks: 0,
-      dialogDelete: false,
-      disableLoad: true,
-      search: "",
-      selection: null,
-      dialogTogglePublic: false,
-    };
-  },
+    const disableLoad = ref(true);
+    const search = ref("");
 
-  computed: {
-    ...mapGetters({
-      visible: "UI_SHOW_LOAD_DIALOG",
-      meta: "VIEW_META",
-      views: "VIEW_LIST_ALL",
-    }),
-    loadDialog: {
-      get() {
-        return this.visible;
-      },
-      set(value) {
-        this.setShowLoadDialog(value);
-        if (!value) {
-          this.$emit("close");
-        }
-      },
-    },
-    headers() {
+    // Table headers
+    const headers = computed(() => {
       return [
         { text: "View Name", value: "name" },
         { text: "First Name", value: "creatorFirst" },
@@ -40,143 +17,193 @@ export default {
         { text: "Date Created", value: "created" },
         { text: "Actions", value: "actions", sortable: false },
       ];
-    },
-    filteredViews() {
-      return this.views.filter((item) => {
-        const notAutoSave = this.viewIsNotAutoSave(item);
-        const isPublic = this.viewIsPublic(item);
-        const createdByUser = this.viewCreatedByUser(item);
-        if (parseInt(this.activeTab) === 0) {
+    });
+
+    // LoadDialog visiblity status
+    const { visible } = useGetters({ visible: "UI_SHOW_LOAD_DIALOG" });
+    const { setShowLoadDialog } = useMutations({
+      setShowLoadDialog: "UI_SHOW_LOAD_DIALOG_SET",
+    });
+    const loadDialog = computed({
+      get: () => visible.value,
+      set: (value) => {
+        setShowLoadDialog(value);
+        if (!value) {
+          emit("close");
+        }
+      },
+    });
+
+    // Filter the avilable views
+    const activeTab = ref(0);
+    const { views } = useGetters({ views: "VIEW_LIST_ALL" });
+    function viewIsNotAutoSave(item) {
+      const userId = girderRest.user._id;
+      // Safe to assume the user did not create this view and it is an
+      // auto-saved view for a run if it contains their userId
+      return !item.name.includes(`_${userId}`);
+    }
+    function viewIsPublic(item) {
+      return item ? item.public : false;
+    }
+    function viewCreatedByUser(item) {
+      return girderRest.user._id === item.creatorId;
+    }
+    const filteredViews = computed(() => {
+      return views.value.filter((item) => {
+        const notAutoSave = viewIsNotAutoSave(item);
+        const isPublic = viewIsPublic(item);
+        const createdByUser = viewCreatedByUser(item);
+        if (parseInt(activeTab.value) === 0) {
           return notAutoSave && (isPublic || createdByUser);
         } else {
           return notAutoSave && createdByUser;
         }
       });
-    },
-  },
+    });
 
-  methods: {
-    ...mapActions({
-      fetchAllViews: "VIEW_FETCH_ALL_AVAILABLE",
-      loadView: "VIEW_LOADED",
-    }),
-    ...mapMutations({
-      setShowLoadDialog: "UI_SHOW_LOAD_DIALOG_SET",
-    }),
-    clearSelection() {
-      this.selection = null;
-      if (this.visible) {
-        this.setShowLoadDialog(false);
+    // Support selecting row, clearing selection
+    const selection = ref(null);
+    const clicks = ref(0);
+    function clearSelection() {
+      selection.value = null;
+      if (visible) {
+        setShowLoadDialog(false);
       }
-    },
-    async deleteView() {
-      this.dialogDelete = false;
-      await this.girderRest
-        .delete(`/view/${this.selection._id}`)
-        .then(() => {
-          this.fetchAllViews();
-          this.selection = null;
-        })
-        .catch((error) => {
-          console.log("error: ", error);
-        });
-    },
-    load() {
-      this.loadView(this.selection);
-      this.clearSelection();
-    },
-    async loadAsTemplate() {
+    }
+    function rowSelected(selectedRow) {
+      clicks.value++;
+      selection.value = selectedRow;
+      if (clicks.value === 1) {
+        setTimeout(() => {
+          clicks.value = 0;
+        }, 200);
+      } else {
+        clicks.value = 0;
+        load();
+      }
+    }
+    function rowClass(item) {
+      if (selection && selection.value && selection.value._id === item._id) {
+        return "selectedRow";
+      } else {
+        return "";
+      }
+    }
+
+    // Load the user selected view
+    const { loadView } = useActions({ loadView: "VIEW_LOADED" });
+    const { meta } = useGetters({ meta: "VIEW_META" });
+    const canBeTemplate = ref(false);
+    function load() {
+      loadView(selection ? selection.value : selection);
+      clearSelection();
+    }
+    function loadSelectedRow(_event, rowSelection) {
+      selection.value = rowSelection.item;
+      load();
+    }
+    watch(selection, (selected) => {
+      canBeTemplate.value = false;
+      if (selected && meta.value) {
+        const { simulation, run } = selected.meta;
+        const sameSimulation = simulation === meta.value.simulation;
+        const differentRun = run !== meta.value.run;
+        canBeTemplate.value = sameSimulation && differentRun;
+      }
+    });
+    async function loadAsTemplate() {
       // Create a list of selection's item ids
       let templateView = {
-        rows: this.selection.rows,
-        columns: this.selection.columns,
+        rows: selection.value.rows,
+        columns: selection.value.columns,
         items: {},
         step: 1,
-        meta: { ...this.meta },
+        meta: { ...meta.value },
       };
 
       async function asyncForEach(keys, values, callback) {
         for (let index = 0; index < values.length; index++) {
           const data = await callback(values[index]);
-          templateView.items[`${keys[index]}`] = data;
+          templateView.items[`${keys[index]}`] = { id: data, zoom: null };
         }
       }
 
-      const keys = Object.keys(this.selection.items);
-      const values = Object.values(this.selection.items);
+      const keys = Object.keys(selection.value.items);
+      const values = Object.values(selection.value.items);
       await asyncForEach(keys, values, async (value) => {
-        const response = await this.girderRest.get(`/item/${value}`);
-        const run = this.meta.run;
+        console.log("value: ", value);
+        const response = await girderRest.get(`/item/${value.id}`);
+        const run = meta.value.run;
         const name = response.data.name;
         const endpoint = `/resource/${run}/search?type=folder&q=${name}`;
-        const result = await this.girderRest.get(endpoint);
+        const result = await girderRest.get(endpoint);
         if (result.data && result.data.results.length) {
           return result.data.results[0].value._id;
         }
         return null;
       });
 
-      this.loadView(templateView);
-      this.clearSelection();
-    },
-    rowSelected(selection) {
-      this.clicks++;
-      this.selection = selection;
-      if (this.clicks === 1) {
-        setTimeout(() => {
-          this.clicks = 0;
-        }, 200);
-      } else {
-        this.clicks = 0;
-        this.load();
-      }
-    },
-    loadSelectedRow(_event, selection) {
-      this.selection = selection.item;
-      this.load();
-    },
-    rowClass(item) {
-      if (this.selection && this.selection._id === item._id) {
-        return "selectedRow";
-      } else {
-        return "";
-      }
-    },
-    viewCreatedByUser(item) {
-      return this.girderRest.user._id === item.creatorId;
-    },
-    viewIsPublic(item) {
-      if (item) {
-        return item.public;
-      }
-      return false;
-    },
-    viewIsNotAutoSave(item) {
-      const userId = this.girderRest.user._id;
-      // Safe to assume the user did not create this view and it is an
-      // auto-saved view for a run if it contains their userId
-      const nameIncludes = item.name.includes(`_${userId}`);
-      return !nameIncludes;
-    },
-    async toggleViewStatus() {
-      this.dialogTogglePublic = false;
-      var formData = new FormData();
-      formData.set("public", !this.selection.public);
-      await this.girderRest
-        .put(`/view/${this.selection._id}`, formData)
+      loadView(templateView);
+      clearSelection();
+    }
+
+    // Delete user created view
+    const dialogDelete = ref(false);
+    const { fetchAllViews } = useActions({
+      fetchAllViews: "VIEW_FETCH_ALL_AVAILABLE",
+    });
+    async function deleteView() {
+      dialogDelete.value = false;
+      await girderRest
+        .delete(`/view/${selection.value._id}`)
         .then(() => {
-          this.fetchAllViews();
+          fetchAllViews();
+          selection.value = null;
         })
         .catch((error) => {
           console.log("error: ", error);
         });
-    },
-    canBeTemplate() {
-      if (this.selection && this.meta) {
-        const { simulation, run } = this.selection.meta;
-        return simulation === this.meta.simulation && run !== this.meta.run;
-      }
-      return false;
-    },
+    }
+
+    // Toggle public/private status of view
+    const dialogTogglePublic = ref(false);
+    async function toggleViewStatus() {
+      dialogTogglePublic.value = false;
+      var formData = new FormData();
+      formData.set("public", !selection.value.public);
+      await girderRest
+        .put(`/view/${selection.value._id}`, formData)
+        .then(() => {
+          fetchAllViews();
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        });
+    }
+
+    return {
+      activeTab,
+      canBeTemplate,
+      clearSelection,
+      deleteView,
+      dialogDelete,
+      dialogTogglePublic,
+      disableLoad,
+      filteredViews,
+      headers,
+      load,
+      loadAsTemplate,
+      loadDialog,
+      loadSelectedRow,
+      rowClass,
+      rowSelected,
+      search,
+      selection,
+      toggleViewStatus,
+      viewCreatedByUser,
+      viewIsPublic,
+      views,
+    };
   },
 };
