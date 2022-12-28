92d39f45f4ed2c5e78f42024ad823795bac1927a
Brianna Major <brianna.major@kitware.com>
2022-12-28 13:28:09 -0500

    Do not use VUE_FOLDER_ID
    This env var required different containers for different deployments. We can
    keep all deployments on the same image by searching for the top-level folder id
    rather than hard-coding it.

--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./components/core/App";
 import Vuex from "vuex";
 import Girder, { RestClient, vuetify } from "@girder/components/src";
-import { isNil } from "lodash";
 import VueMathjax from "vue-mathjax";
 import store from "./store";
 
@@ -18,13 +17,19 @@ export function bootstrap() {
     fastRestUrl = `http://localhost:5000/api/v1`;
   }
 
-  let folderId = null;
-  if (!isNil(process.env.VUE_APP_FOLDER_ID)) {
-    folderId = process.env.VUE_APP_FOLDER_ID;
-  }
-
+  const defaultLocation = { _modelType: "folder", _id: null };
   const girderRest = new RestClient({ apiRoot, authenticateWithCredentials });
-  const defaultLocation = { _modelType: "folder", _id: folderId };
+  girderRest.get("/collection?text=eSimMon").then((collections) => {
+    // The dashboard will always use the eSimMon collection to collect all data
+    let id = collections.data[0]._id;
+    girderRest
+      .get(`/folder?parentType=collection&parentId=${id}`)
+      .then((folders) => {
+        // The eSimMon collection should have a single folder that contains all
+        // data and serves as the top-level directory.
+        defaultLocation._id = folders.data[0]._id;
+      });
+  });
 
   Vue.config.productionTip = false;
   Vue.use(Vuex);
