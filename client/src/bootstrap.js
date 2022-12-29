547419050a62caf36f8a523d2c05dcf9a65d5237
Brianna Major <brianna.major@kitware.com>
2022-12-29 14:43:28 -0500

    Set URLs based on current deployment
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- client/src/bootstrap.js
+++ client/src/bootstrap.js
@@ -10,11 +10,21 @@ export function bootstrap() {
   Vue.use(VueMathjax);
 
   let apiRoot = `${window.location}api/v1`;
-  let authenticateWithCredentials = false;
+  let fastRestUrl = "http://localhost:5000/api/v1";
+  let authenticateWithCredentials = true;
 
-  var fastRestUrl = apiRoot;
-  if (window.location.host === "esimmon.kitware.com") {
-    fastRestUrl = `http://localhost:5000/api/v1`;
+  let deployment = window.location.hostname.split(".");
+  deployment = deployment[deployment.length - 2];
+  if (deployment === "kitware") {
+    // AWS Deployment
+    apiRoot = `https://api.${window.location.hostname}/api/v1`;
+    fastRestUrl = apiRoot;
+  } else if (deployment === "nersc") {
+    // NERSC Deployment
+    apiRoot = `https://${window.location.hostname}/api/v1`;
+    fastRestUrl = apiRoot;
+  } else {
+    authenticateWithCredentials = false;
   }
 
   const defaultLocation = { _modelType: "folder", _id: null };
