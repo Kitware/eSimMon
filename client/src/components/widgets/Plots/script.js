f4bdd82f0e16355872c902044933ee6a9c4ec14d
Brianna Major <brianna.major@kitware.com>
2023-02-08 13:05:10 -0500

    Do not try to save plot data we already have
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- client/src/components/widgets/Plots/script.js
+++ client/src/components/widgets/Plots/script.js
@@ -166,31 +166,33 @@ export default {
         reader.onload = () => {
           let img;
           const ltsd = this.loadedTimeStepData();
-          if (this.plotType === PlotType.VTK) {
-            img = decode(reader.result);
-            this.$refs[`${this.row}-${this.col}`].addRenderer(img);
-            this.setLoadedTimeStepData({
-              [`${this.itemId}`]: [
-                ...ltsd,
-                {
-                  timestep: timeStep,
-                  data: img,
-                },
-              ],
-            });
-          } else {
-            img = JSON.parse(reader.result);
-            this.setLoadedTimeStepData({
-              [`${this.itemId}`]: [
-                ...ltsd,
-                {
-                  timestep: timeStep,
-                  data: img.data,
-                  layout: img.layout,
-                  type: img.type,
-                },
-              ],
-            });
+          if (!this.isTimeStepLoaded(timeStep)) {
+            if (this.plotType === PlotType.VTK) {
+              img = decode(reader.result);
+              this.$refs[`${this.row}-${this.col}`].addRenderer(img);
+              this.setLoadedTimeStepData({
+                [`${this.itemId}`]: [
+                  ...ltsd,
+                  {
+                    timestep: timeStep,
+                    data: img,
+                  },
+                ],
+              });
+            } else {
+              img = JSON.parse(reader.result);
+              this.setLoadedTimeStepData({
+                [`${this.itemId}`]: [
+                  ...ltsd,
+                  {
+                    timestep: timeStep,
+                    data: img.data,
+                    layout: img.layout,
+                    type: img.type,
+                  },
+                ],
+              });
+            }
           }
           return resolve(img);
         };
