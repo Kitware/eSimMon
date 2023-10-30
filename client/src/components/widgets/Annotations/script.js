57db4ee9a94cb939eceb359161c766c12882fa56
Brianna Major <brianna.major@kitware.com>
2023-10-30 16:50:07 -0400

    Add an annotation indicating the current time step per plot
    Signed-off-by: Brianna Major <brianna.major@kitware.com>

--- client/src/components/widgets/Annotations/script.js
+++ client/src/components/widgets/Annotations/script.js
@@ -24,6 +24,10 @@ export default {
       type: Boolean,
       default: false,
     },
+    size: {
+      type: String,
+      default: "smaller",
+    },
   },
   computed: {
     ...mapGetters({
